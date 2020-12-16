a19a1aa5838442c54df7ab5dba447289f759e7f8
Frank Guchelaar <fguchelaar@gmail.com>
2020-12-16 14:32:52 +0100

    finished day 16

--- day-16-ticket-translation/Sources/Main/main.swift
+++ day-16-ticket-translation/Sources/Main/main.swift
@@ -1,7 +1,8 @@
 import Puzzle
 import AdventKit
 
-let puzzle = Puzzle(input: try! String(contentsOfFile: "input.txt"))
+//let puzzle = Puzzle(input: try! String(contentsOfFile: "input.txt"))
+let puzzle = Puzzle(input: try! String(contentsOfFile: "/Users/frank/Workspace/github/fguchelaar/AdventOfCode2020/day-16-ticket-translation/input.txt"))
 
 print("Part 1: \(puzzle.part1())")
 print("Part 2: \(puzzle.part2())")
--- day-16-ticket-translation/Sources/Puzzle/Puzzle.swift
+++ day-16-ticket-translation/Sources/Puzzle/Puzzle.swift
@@ -1,7 +1,7 @@
 import AdventKit
 import Foundation
 
-struct Rule {
+struct Rule: Hashable {
     let key: String
     let lower1, lower2, upper1, upper2: Int
 
@@ -55,7 +55,43 @@ public class Puzzle {
             .reduce(0, +)
     }
 
-    public func part2() -> String {
-        ""
+    public func part2() -> Int {
+        // 1. Discard all invalid tickets
+        let valid = nearby
+            .filter { ticket in ticket.allSatisfy { value in rules.contains { rule in rule.isValid(value) }} }
+
+        // 2. Find the eligible rules for each 'value-index'
+        let numberOfValues = valid.first!.count
+        var eligible = [Rule: [Int]]()
+        (0 ..< numberOfValues).forEach { index in
+            let valuesAtIndex = valid.map { $0[index] }
+            let validRulesAtIndex = rules.filter { rule in valuesAtIndex.allSatisfy { value in rule.isValid(value) }}
+
+            for rule in validRulesAtIndex {
+                eligible[rule] = (eligible[rule] ?? []) + [index]
+            }
+        }
+
+        // 3. Make each eligible set as small as possbible by eliminating all
+        //    indexes that only have one possible rule.
+        //    Repeat #3 until fields are found (have only one eligible rule)
+        while !eligible.values.allSatisfy({ $0.count == 1 }) {
+            // this can be quicker, by removing the rules with only one option
+            // from the eligible and putting then in a seperate dictionary
+            let allSingles = eligible.values
+                .filter { values in values.count == 1 }.flatMap { $0 }
+
+            // eliminate all found indexes from the rules, skipping the ones
+            // that only have one option; they're done already
+            for key in eligible.filter({ $0.value.count != 1 }).keys {
+                eligible[key] = eligible[key]!.filter { !allSingles.contains($0) }
+            }
+        }
+
+        return eligible
+            .filter { $0.key.key.hasPrefix("departure") }
+            .values
+            .flatMap { $0 }
+            .reduce(1) { $0 * ticket[$1] }
     }
 }
